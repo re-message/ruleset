ca97aa0217d0c1fc9b75ec84fd1d3ed091cf1194
Oleg Kozlov <h1karo@remessage.ru>
2022-04-23 20:22:31 +0300

    Use custom config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -40,37 +40,11 @@ $header = <<<EOF
     file that was distributed with this source code.
     EOF;
 
-$config = new PhpCsFixer\Config();
+$config = new RM\Style\RuleSet\Config();
 
 return $config
     ->setRules(
         [
-            '@PSR12' => true,
-            '@Symfony' => true,
-            '@PhpCsFixer' => true,
-            '@DoctrineAnnotation' => true,
-            '@PHP80Migration:risky' => true,
-            '@PHP81Migration' => true,
-            'ordered_imports' => [
-                'sort_algorithm' => 'alpha',
-                'imports_order' => ['class', 'function', 'const'],
-            ],
-            'declare_strict_types' => false,
-            'ordered_class_elements' => false,
-            'no_superfluous_phpdoc_tags' => false,
-            'strict_param' => true,
-            'array_syntax' => ['syntax' => 'short'],
-            'concat_space' => ['spacing' => 'one'],
-            'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
-            'phpdoc_tag_type' => [
-                'tags' => ['inheritDoc' => 'annotation'],
-            ],
-            'phpdoc_tag_casing' => [
-                'tags' => ['inheritDoc'],
-            ],
-            'phpdoc_to_comment' => [
-                'ignored_tags' => ['todo', 'noinspection', 'psalm-suppress'],
-            ],
             'header_comment' => [
                 'header' => $header,
                 'comment_type' => 'comment',
@@ -79,6 +53,5 @@ return $config
             ],
         ]
     )
-    ->setRiskyAllowed(true)
     ->setFinder($finder)
 ;
